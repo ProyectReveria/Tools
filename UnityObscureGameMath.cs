7faeb14c0facc64b7b3a5ea087aaa93476f218fa
P.Revery By Uriel "Rosary" Gomez <Brokenlevenheart@gmail.com>
2026-05-02 17:22:47 -0600

    Update UnityObscureGameMath.cs
    Combatibility whit Unity FIX

--- UnityObscureGameMath.cs
+++ UnityObscureGameMath.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
-namespace UnityEngine.Obscure_Framework.Game_Math; 
+using System; 
 
-public static class ObscureMath 
+namespace UnityEngine.Obscure_Framework.Game_Math
 {
+    public static class ObscureMath
+    {
     private static readonly System.Random _Sran_ = new System.Random(); 
     public static double incrementalXP(double xp_i,double xp_f) => (xp_f - xp_i / xp_i) * 100; 
     public static double inflation(double Current_CPI, double Previous_CPI) => (Current_CPI - Previous_CPI / Previous_CPI) * 100; 
@@ -17,4 +19,8 @@ public static class ObscureMath
         return _Sran_.NextDouble() * (max-min) + max; 
     }
     public static double Crit_Change_Calculation_Or_CritDamage(double base_crit_chance, double aditive_Crit) => base_crit_chance * (1+aditive_Crit);
+    }
+
 }
+
+
